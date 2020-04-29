e6095949b561abda28b2081b2d8fad4f5a9ed88d
andreivaino@live.ru <andreivaino@live.ru>
2020-04-29 13:37:33 +0300

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { MainComponent } from './components/main/main.component';
+import { NewCurrencyComponent } from './components/new-currency/new-currency.component';
+import { EditCurrencyComponent } from './components/edit-currency/edit-currency.component';
+import {OwnFilter} from './pipes/OwnFilter';
+import {RouterModule} from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {DatePipe} from '@angular/common';
+import {PipesModule} from 'pipes-module';
+import {NgSelectModule} from '@ng-select/ng-select';
+import {NgbAlertModule, NgbModule, NgbPaginationModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    NewCurrencyComponent,
+    EditCurrencyComponent,
+    OwnFilter
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    PipesModule,
+    NgSelectModule,
+    NgbPaginationModule,
+    NgbAlertModule,
+    NgbModule
   ],
-  providers: [],
+  providers: [
+    DatePipe
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
